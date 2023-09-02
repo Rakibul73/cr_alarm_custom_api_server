6ee4589cc87121ec817e317e11132c4630c67d34
Rakibul73 <rakibul29185@yahoo.com>
2023-09-02 15:58:48 +0600

    added explanation

--- api.py
+++ api.py
@@ -3,6 +3,7 @@ import datetime
 
 app = Flask(__name__)
 
+# Global variables
 timestamp = None
 text = None
 
@@ -10,24 +11,61 @@ text = None
 def hello_world():
     return 'Hello from Flask! it is hosted in pythonanywhere'
 
-@app.route("/get_timestamp" , methods=["GET"])
+@app.route("/get_timestamp", methods=["GET"])
 def get_timestamp():
-    global timestamp , text
-    return jsonify({"timestamp": timestamp , "text": text})
+    """
+    Get the timestamp and text that is stored in the previous POST request.
+
+    Parameters:
+        None
+
+    Returns:
+        dict: A dictionary containing the timestamp and text.
+            - timestamp (int): The timestamp.
+            - text (str): The text.
+    """
+    global timestamp, text
+    return jsonify({
+        "timestamp": timestamp,
+        "text": text
+    })
 
 
 
 @app.route("/add_data", methods=["POST"])
 def add_data():
+    """
+    Handles the route "/add_data" for the POST method.
+    
+    This function is responsible for adding data to the system. It retrieves the input value
+    from the request parameters and stores it in the 'text' variable. It also updates the 
+    'timestamp' variable with the current date and time when post request is made. Finally, 
+    it returns a JSON response indicating the success of the operation.
+    
+    Parameters:
+        key = input, value=<your input value>
+        
+    Returns:
+        A JSON response with the key "success" set to True.
+    """
     global timestamp , text
     timestamp = datetime.datetime.now()
-    text = str(request.args.get('input'))  # request like this zzz.com/add_data?input=
+    text = str(request.args.get('input'))  
     return jsonify({"success": True})
 
 
 
 @app.route("/add_data_get" , methods=["GET"])
 def add_data_get():
+    """
+    Handle GET requests to the /add_data_get endpoint.
+    
+    It updates the 'timestamp' variable with the current date and time when post request is made.
+    Finally, it returns a JSON response indicating the success of the operation.
+
+    Returns:
+        A JSON response containing the current timestamp and text.
+    """
     global timestamp
     timestamp = datetime.datetime.now()
     return jsonify({"timestamp": timestamp , "text": text})
