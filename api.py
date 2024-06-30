15e5e5359af3b671b47393f600e15467c2754ed4
Md. Rakibul Islam <72851692+Rakibul73@users.noreply.github.com>
2024-06-30 21:40:00 +0600

    Update api.py

--- api.py
+++ api.py
@@ -21,7 +21,7 @@ def git_update():
     """
     repo = git.Repo('./cr_alarm_custom_api_server')
     origin = repo.remotes.origin
-    repo.create_head('main', origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+    # repo.create_head('main', origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
     origin.pull()
     return '', 200
 
